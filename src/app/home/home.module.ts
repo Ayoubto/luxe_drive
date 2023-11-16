a76242a207d5f4379417e00267804f79c038508a
ayoub toubi <ayoub123toubi@gmail.com>
2023-11-16 18:43:28 +0100

    Components-Search

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,18 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { SharedModule } from '../shared/shared.module';
+import { MarquesComponent } from './components/marques/marques.component';
+import { SearchComponent } from './components/search/search.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    MarquesComponent,
+    SearchComponent
   ],
   imports: [
     CommonModule
   ],
   exports:[
-    HomeComponent
+    HomeComponent,
+    MarquesComponent,
+    SearchComponent
+
   ]
 })
 export class HomeModule { }
