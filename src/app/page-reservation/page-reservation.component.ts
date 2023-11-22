abdb98ac4656c9dc80d11401b629f546b658bd2e
Firdaous <firdaous.boulben@gmail.com>
2023-11-22 20:08:02 +0100

    Feedbacks section update

--- src/app/page-reservation/page-reservation.component.ts
+++ src/app/page-reservation/page-reservation.component.ts
@@ -9,12 +9,12 @@ export class PageReservationComponent {
 
 
   Cars:Array<any>=[
-    {img:"Audi A8 L 2022" , Name:"Audi A8 L 2022" , price:75.90 , speedometer:"4,00", manual_gearbox:"Auto",users:"4 person",gas_station:"Electirc",},
-    {img:"Nissan Maxima Platinum" , Name:"Nissan Maxima Platinum 2022" , price:75.90 , speedometer:"4,00", manual_gearbox:"Auto",users:"4 person",gas_station:"Electirc",},
-    {img:"Porsche Cayenne GTS 2022" , Name:"Porsche Cayenne GTS 2022" , price:75.90 , speedometer:"4,00", manual_gearbox:"Auto",users:"4 person",gas_station:"Electirc",},
-    {img:"BMW M8 Coupe 2022" , Name:"BMW M8 Coupe 2022" , price:75.90 , speedometer:"4,00", manual_gearbox:"Auto",users:"4 person",gas_station:"Electirc",},
-    {img:"BMW X7 M50i 2022" , Name:"BMW X7 M50i 2022" , price:75.90 , speedometer:"4,00", manual_gearbox:"Auto",users:"4 person",gas_station:"Electirc",},
-    {img:"Porsche Cayenne GTS 2023" , Name:"Porsche Cayenne GTS 2023" , price:75.90 , speedometer:"4,00", manual_gearbox:"Auto",users:"4 person",gas_station:"Electirc",}
+    {img:"Audi A8 L 2022" , Name:"Audi A8 L 2022" , price:75.90 , speedometer:"4,00", manual_gearbox:"Auto",users:"4 Personne",gas_station:"Electrique",},
+    {img:"Nissan Maxima Platinum" , Name:"Nissan Maxima Platinum 2022" , price:75.90 , speedometer:"4,00", manual_gearbox:"Auto",users:"4 Personne",gas_station:"Electrique",},
+    {img:"Porsche Cayenne GTS 2022" , Name:"Porsche Cayenne GTS 2022" , price:75.90 , speedometer:"4,00", manual_gearbox:"Auto",users:"4 Personne",gas_station:"Electrique",},
+    {img:"BMW M8 Coupe 2022" , Name:"BMW M8 Coupe 2022" , price:75.90 , speedometer:"4,00", manual_gearbox:"Auto",users:"4 Personne",gas_station:"Electrique",},
+    {img:"BMW X7 M50i 2022" , Name:"BMW X7 M50i 2022" , price:75.90 , speedometer:"4,00", manual_gearbox:"Auto",users:"4 Personne",gas_station:"Electrique",},
+    {img:"Porsche Cayenne GTS 2023" , Name:"Porsche Cayenne GTS 2023" , price:75.90 , speedometer:"4,00", manual_gearbox:"Auto",users:"4 Personne",gas_station:"Electrique",}
 
   ]
   items = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
