6d854eb0823893cde3d2aae1e8792f55c62d7cbd
ayoub toubi <ayoub123toubi@gmail.com>
2023-12-25 22:44:49 +0100

    add routing to compte

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { PageContactComponent } from './page-contact/page-contact.component';
 import { PageAgencesComponent } from './page-agences/page-agences.component';
 import { PageReservationComponent } from './page-reservation/page-reservation.component';
 import { PageHomeComponent } from './page-home/page-home.component';
-
+import {ComptesComponent} from './cote_admin/gestion-comptes/comptes/comptes.component'
+import {CreerCompteComponent} from './cote_admin/gestion-comptes/creer-compte/creer-compte.component'
 const routes: Routes = [
   {path:"home",component:PageHomeComponent},
   {path:"contact",component:PageContactComponent},
   {path:"agences",component:PageAgencesComponent},
   {path:"reservation",component:PageReservationComponent},
+  {path:"accueil/gestion-des-comptes",component:ComptesComponent},
+  {path:"accueil/ajouter-compte",component:CreerCompteComponent},
   {path:"**",redirectTo:"home",pathMatch:"full"}
 ];
 
