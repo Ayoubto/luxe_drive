22243df6e5395144b8a33b6d9408d751e750384c
ayoub toubi <ayoub123toubi@gmail.com>
2023-12-29 20:51:40 +0100

    gestion des voiture

--- src/app/cote_admin/gestion-voitures/ajouter-voiture/ajouter-voiture.component.ts
+++ src/app/cote_admin/gestion-voitures/ajouter-voiture/ajouter-voiture.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { faEye, faEyeSlash,faDoorOpen ,faCog  ,faTrademark ,faCubes,faLayerGroup,faMoneyBill ,faTachometerAlt } from '@fortawesome/free-solid-svg-icons';
+import { ActivatedRoute } from '@angular/router';
+import {ClientService} from '../../../client.service'
+
 
 @Component({
   selector: 'app-ajouter-voiture',
@@ -7,4 +12,127 @@ import { Component } from '@angular/core';
 })
 export class AjouterVoitureComponent {
   Page_Titre="Gestion des Voitures"
+
+
+ // lire Id 
+ hasIdParam: boolean=false;
+  
+ id: number | null=null;
+ responseData: any={};
+
+
+defaultImage = '../../../../assets/images/voiture.png';
+currentImage: string = this.defaultImage;
+
+onAddImage() {
+ const fileInput = document.createElement('input');
+ fileInput.type = 'file';
+ fileInput.accept = 'image/*';
+ fileInput.click();
+
+ fileInput.addEventListener('change', (event: any) => {
+   const file = event.target.files[0];
+   if (file) {
+     const reader = new FileReader();
+     reader.onload = (e: any) => {
+       this.currentImage = e.target.result;
+     };
+     reader.readAsDataURL(file);
+   }
+ });
+}
+
+onRemoveImage() {
+ this.currentImage = this.defaultImage;
+}
+
+creerCompte: FormGroup;
+
+constructor(private formBuilder: FormBuilder,private route: ActivatedRoute,private ClientService: ClientService) {
+ this.creerCompte = this.formBuilder.group({
+   marque: ['', Validators.required],
+   modele: ['', Validators.required],
+   quantite : ['', [Validators.required]],
+   Portes_Energie: ['', [Validators.required, Validators.email]],
+   capacity: ['', [Validators.required,]],
+   boite: ['', Validators.required],
+   consommation: ['', Validators.required],
+   prix: ['', Validators.required],
+ }, {
+   validators: this.passwordMatchValidator
+ });
+
+}
+
+passwordMatchValidator(group: FormGroup): { [key: string]: any } | null {
+ const password = group.get('password')?.value;
+ const confirmPwd = group.get('confirmPwd')?.value;
+
+ return password === confirmPwd ? null : { 'passwordMismatch': true };
+}
+
+ngOnInit() {
+ this.route.params.subscribe(params => {
+   const id = params['id'];
+   this.id = id ? Number(id) : null;
+   
+   if (this.id !== null) {
+     this.fetchDataById(this.id);
+   } else {
+     this.responseData = null;
+
+   }
+
+ });
+ 
+}
+fetchDataById(id: number): void {
+ this.ClientService.getDataById(id).subscribe(
+   (data) => {
+     this.responseData = data; // Assign fetched data to formData
+     
+   },
+   (error) => {
+     console.error('Error fetching data:', error);
+     this.responseData = {ERROR:"jfjfjf"}; // Set formData as empty in case of an error
+   
+   }
+ );
+ console.log(this.responseData)
+}
+
+onSubmit() {
+ // Handle form submission logic here
+ console.log('Form submitted:', this.creerCompte.value);
+}
+onSubmitNotEmpty(){
+ console.log('Form update:', this.creerCompte.value);
+
+}
+
+//Font Awesome icons
+
+
+eyePwd = faEye;
+
+marque=faTrademark
+modele=faCubes
+Quantity=faLayerGroup 
+eyeConfirmPwd = faEye;
+energy=faDoorOpen ;
+boite=faCog  
+price=faMoneyBill 
+concommation=faTachometerAlt 
+hidePassword = true;
+hideConfirmPassword = true;
+
+togglePasswordVisibility(): void {
+ this.hidePassword = !this.hidePassword;
+ this.eyePwd = this.hidePassword ? faEye : faEyeSlash;
+}
+
+toggleConfirmPasswordVisibility(): void {
+ this.hideConfirmPassword = !this.hideConfirmPassword;
+ this.eyeConfirmPwd = this.hideConfirmPassword ? faEye : faEyeSlash;
+}
 }
--- src/app/cote_admin/gestion-voitures/voitures/voitures.component.ts
+++ src/app/cote_admin/gestion-voitures/voitures/voitures.component.ts
@@ -1,10 +1,157 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ViewEncapsulation ,ViewChild} from '@angular/core';
+import {MatPaginator,MatPaginatorIntl , MatPaginatorModule} from '@angular/material/paginator';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import { faPencil, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { MatSort } from '@angular/material/sort';
+
+
 
 @Component({
   selector: 'app-voitures',
   templateUrl: './voitures.component.html',
-  styleUrls: ['./voitures.component.css']
+  styleUrls: ['./voitures.component.css'],
+  encapsulation: ViewEncapsulation.None // Set encapsulation to None
 })
 export class VoituresComponent {
   Page_Titre="Gestion des Voitures"
+
+    // Manually created data
+    responseData:PeriodicElement[]=[
+      {
+        image: '../../../../assets/images/Cart-Cars/Cars/BMW M8 Coupe 2022.svg', 
+        marque: 'Nissan', 
+        modele: 'Maxima Platinum 2022', 
+        capacity: "4 persone", 
+        quantite: 12, 
+        boite: 'automatique', 
+        Portes_Energie:"Elictric",
+        consommation:"4.2",
+        prix:200,
+        actions: 'BMW X7 M50i 2022' 
+
+      },
+      {
+        image: '../../../../assets/images/Cart-Cars/Cars/Nissan Maxima Platinum.svg', 
+        marque: 'Nissan', 
+        modele: 'Maxima Platinum 2022', 
+        capacity: "4 persone", 
+        quantite: 12, 
+        boite: 'automatique', 
+        Portes_Energie:"Elictric",
+        consommation:"4.2",
+        prix:200,
+        actions: '' 
+
+      },      {
+        image: '../../../../assets/images/Cart-Cars/Cars/Porsche Cayenne GTS 2022.svg', 
+        marque: 'Nissan', 
+        modele: 'Maxima Platinum 2022', 
+        capacity: "4 persone", 
+        quantite: 12, 
+        boite: 'automatique', 
+        Portes_Energie:"Elictric",
+        consommation:"4.2",
+        prix:200,
+        actions: '' 
+
+      },      {
+        image: '../../../../assets/images/Cart-Cars/Cars/Porsche Cayenne GTS 2023.svg', 
+        marque: 'Nissan', 
+        modele: 'Maxima Platinum 2022', 
+        capacity: "4 persone", 
+        quantite: 12, 
+        boite: 'automatique', 
+        Portes_Energie:"Elictric",
+        consommation:"4.2",
+        prix:200,
+        actions: '' 
+
+      },      {
+        image: '../../../../assets/images/Cart-Cars/Cars/BMW M8 Coupe 2022.svg', 
+        marque: 'Nissan', 
+        modele: 'Maxima Platinum 2022', 
+        capacity: "4 persone", 
+        quantite: 12, 
+        boite: 'automatique', 
+        Portes_Energie:"Elictric",
+        consommation:"4.2",
+        prix:200,
+        actions: '' 
+
+      },      {
+        image: '../../../../assets/images/Cart-Cars/Cars/Audi A8 L 2022.svg', 
+        marque: 'Nissan', 
+        modele: 'Maxima Platinum 2022', 
+        capacity: "4 persone", 
+        quantite: 12, 
+        boite: 'automatique', 
+        Portes_Energie:"Elictric",
+        consommation:"4.2",
+        prix:200,
+        actions: '' 
+
+      }
+    ]
+
+
+
+
+      // Rechercher
+  inputValue: string = '';
+  Search() {
+    if (this.inputValue === '') {
+      this.dataSource.data = this.responseData;
+    } else {
+      this.dataSource.data = this.responseData.filter(cli => this.checkProperties(cli, this.inputValue));
+    }
+  }
+
+  checkProperties(object: any, input: string): boolean {
+    for (const key in object) {
+      if (Object.prototype.hasOwnProperty.call(object, key)) {
+        if (object[key] && object[key].toString().toLowerCase().includes(input.toLowerCase())) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  // Filtrer les résultats
+  selectedValue: string = '';
+  filteredData: any[] = [];
+
+  // Trier
+  @ViewChild(MatSort) sort!: MatSort;
+
+  // Afficher les colonnes
+  displayedColumns: string[] = ['image', 'marque', 'modele', 'info','prix','quantite','actions'];
+  dataSource = new MatTableDataSource<PeriodicElement>(this.responseData);
+
+  // Pagination 
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
+
+  ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  onInputChange() {
+    this.Search();
+  }
+
+  modifier = faPencil;
+  supprimer = faTrash;
+}
+export interface PeriodicElement {
+  image:string, 
+  marque: string, 
+  modele:string, 
+  capacity:string, 
+  quantite: number, 
+  boite: string, 
+  Portes_Energie:string,
+  consommation:string,
+  prix:number,
+  actions: string;
 }
