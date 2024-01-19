8352a29866541cf07b56c29635c1b341c7da4bf6
Firdaous <firdaous.boulben@gmail.com>
2024-01-19 15:43:48 +0100

    Historique des réservations des clients

---
+++ src/app/services/historique.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HistoriqueService {
+  constructor(private http: HttpClient) { }
+  private apiUrl = 'http://localhost:8093';
+
+}
