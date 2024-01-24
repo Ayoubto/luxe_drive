6237f109760471fec9df0a216798c6f2386e55e4
ayoub toubi <ayoub123toubi@gmail.com>
2024-01-24 17:03:12 +0100

    lien dashbord and flask

--- src/app/cote_admin/dashboard/dashboard.component.ts
+++ src/app/cote_admin/dashboard/dashboard.component.ts
@@ -11,21 +11,44 @@ export class DashboardComponent {
   Page_Titre="Tableau de bord"
   img:any
   imageUrl: string=""
-
+  user_count=""
+  revenue=""
+  reservations_count=""
   constructor(private FlaskService:FlaskService){}
 
-  getChart(){
-    console.log("Hello word")
-    this.FlaskService.getchart().subscribe(
-      rese=>{
-        const bolb:Blob=rese.body as Blob;
-        const url=window.URL.createObjectURL(bolb);
-        window.open(url)
+  getrevenu(){
+    this.FlaskService.getrevenue().subscribe(
+      (rese)=>{
+       this.revenue=rese.total_revenue
+      },
+      error => {
+        console.error('Error fetching user count:', error);
       }
-
     )}
+    getreservation(){
+      this.FlaskService.getreservations_count().subscribe(
+        (rese)=>{
+         this.reservations_count=rese.reservations_count
+        },
+        error => {
+          console.error('Error fetching user count:', error);
+        }
+      )}
+      getUser_count(){
+        this.FlaskService.getuser_count().subscribe(
+          (rese)=>{
+           this.user_count=rese.user_count
+          },
+          error => {
+            console.error('Error fetching user count:', error);
+          }
+        )}
 
   ngOnInit(){
+    this.getUser_count()
+    this.getreservation()
+    this.getrevenu()
+   
    
 }
 }
--- src/app/services/flask.service.ts
+++ src/app/services/flask.service.ts
@@ -9,7 +9,16 @@ export class FlaskService {
   private apiUrl = 'http://127.0.0.1:5000';
   constructor(private http: HttpClient) { }
 
-  getchart():Observable<HttpResponse<Blob>>{
+  getchart():Observable<any>{
     return this.http.get<any>(`${this.apiUrl}/chart?param={"type": "bar"}`);
+  }
+  getuser_count():Observable<any>{
+    return this.http.get<any>(`${this.apiUrl}/user_count`);
+  }
+  getrevenue():Observable<any>{
+    return this.http.get<any>(`${this.apiUrl}/revenue`);
+  }
+  getreservations_count():Observable<any>{
+    return this.http.get<any>(`${this.apiUrl}/reservations_count`);
   }
 }
